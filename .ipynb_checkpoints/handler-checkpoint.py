b4eb1c00c1869dbddec790995196b2853a3e4714
Samuel Silva <samuel.silva.262816@gmail.com>
2023-01-14 17:50:03 -0300

    Updated handler.py - set port and host

--- .ipynb_checkpoints/handler-checkpoint.py
+++ .ipynb_checkpoints/handler-checkpoint.py
@@ -43,4 +43,4 @@ def health_insurance_predict():
 
 if __name__ == '__main__':
     port = os.environ.get( 'PORT', 5000 )
-    app.run( '0.0.0.0', debug=True)
+    app.run( host= '0.0.0.0', port= port)
